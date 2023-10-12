c67c048c0a93bf812996aa2ddc3eba84f3355317
gulya <gulya@STEPAN>
2023-10-12 15:57:21 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int number;
+    cout << "Введите любое число: ";
+    cin >> number;
+    if (number % 2 != 0 and number > 99 and number < 999) {
+        cout << "Истина";
+    }
+    else {
+        cout << "Ложь";
+    }
+}
---
+++ laba3_1/laba3_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Введите число: ";
+    int _number;
+    cin >> _number;
+    if ((_number % 2 == 0) and (_number > 99 and _number < 1000)) {
+        cout << "Истина";
+    }
+    else {
+        cout << "Ложь";
+    }
+}
